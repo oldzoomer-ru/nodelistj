2b64f77c2fe71b20cc14b35d1ae2f8169155cbee
Egor Gavrilov <gavrilovegor519-2@yandex.ru>
2025-03-13 15:59:22 +0300

    Add generate Nodelist from maps.

--- src/main/java/ru/gavrilovegor519/Nodelist.java
+++ src/main/java/ru/gavrilovegor519/Nodelist.java
@@ -10,7 +10,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,6 +41,10 @@ public class Nodelist {
         } catch (IOException e) {
             throw new IllegalArgumentException("Cannot read file", e);
         }
+    }
+
+    public Nodelist(Map<Integer, NodelistEntryDto> entries) {
+        nodelistEntries.putAll(entries);
     }
 
     /**
